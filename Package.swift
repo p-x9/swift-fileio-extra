85e02e69e3997cd5ffcab8ae8b824bf89c000571
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-01-16 03:35:51 +0900

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -3,16 +3,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "FileIOBinary",
+    name: "swift-fileio-extra",
     products: [
         .library(
             name: "FileIOBinary",
             targets: ["FileIOBinary"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/p-x9/swift-fileio.git", from: "0.13.0"),
+        .package(url: "https://github.com/p-x9/swift-binary-parse-support.git", branch: "main")
+    ],
     targets: [
         .target(
-            name: "FileIOBinary"
+            name: "FileIOBinary",
+            dependencies: [
+                .product(name: "FileIO", package: "swift-fileio"),
+                .product(name: "BinaryParseSupport", package: "swift-binary-parse-support")
+            ]
         ),
         .testTarget(
             name: "FileIOBinaryTests",
