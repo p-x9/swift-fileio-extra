2ca59bf9a59fc4a21959c846f9c680c63687b60c
p-x9 <50244599+p-x9@users.noreply.github.com>
2026-02-05 01:55:19 +0900

    Bump `BinaryParseSupport` to 0.2.1

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 
 import PackageDescription
 
-let binaryParseSupportVersion: Version = "0.1.0"
+let binaryParseSupportVersion: Version = "0.2.1 "
 
 let package = Package(
     name: "swift-fileio-extra",
